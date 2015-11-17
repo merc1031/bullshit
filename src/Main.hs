9f43bc2e95346020103bde45581ac8924b8a7f16
Stephen Weeks <sweeks@imvu.com>
2015-11-17 10:24:18 -0800

    Conduit

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,15 @@
 module Main where
-import Control.Concurrent
+import Control.Concurrent (forkIO)
 import Control.Concurrent.Async
-import Control.Monad
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Resource
+import Data.Conduit (Sink, Conduit, Source, await, awaitForever, yield)
+import Data.Conduit.Async
+import qualified Data.Conduit.Combinators as DCC
+import Data.Conduit.Filesystem
 import Data.List
 import Data.Time.Clock
 import Foreign.Marshal.Alloc
-import GHC.Ptr
 import System.Directory
 import System.Environment (getArgs)
 import System.FilePath
@@ -15,50 +19,57 @@ import System.Posix.Files
 bufSize :: Int
 bufSize = 464
 
-worker :: Chan FilePath -> Int -> IO ()
-worker chan _ = allocaBytes bufSize $ \buf ->
-  forever $ do
-    path <- readChan chan
-    contents <- map (path </>) . filter (not . isPrefixOf ".") <$> getDirectoryContents path
-    statted <- zip contents <$> mapConcurrently getFileStatus contents
-    let (dirs, files) = partition (isDirectory . snd) statted
-    writeList2Chan chan $ map fst dirs
-    unless (null files) $ void $ mapConcurrently (readContents buf . fst) files
+readContents :: FilePath -> IO ()
+readContents path = allocaBytes bufSize $ \buf ->
+  withFile path ReadMode $ \h -> do
+    hSetBinaryMode h True
+    hSetBuffering h $ BlockBuffering $ Just bufSize
+    _ <- hGetBuf h buf bufSize
     return ()
 
-readContents :: GHC.Ptr.Ptr a -> FilePath -> IO ()
-readContents buf path = withFile path ReadMode $ \h -> do
-  hSetBinaryMode h True
-  hSetBuffering h $ BlockBuffering $ Just bufSize
-  _ <- hGetBuf h buf bufSize
-  return ()
+sink :: Sink FilePath IO ()
+sink = awaitForever $ \path -> liftIO $ forkIO $ readContents path
 
 mark :: Int
-mark = 1000
+mark = 100
 
-timer :: Chan FilePath -> Int -> UTCTime -> IO ()
-timer mon total time = do
-  _ <- readChan mon
-  if total `mod` mark == 0
-    then do
-      newtime <- getCurrentTime
-      let diff = diffUTCTime newtime time
-          rate = truncate $ fromIntegral mark / diff :: Integer
-      putStrLn $ show total ++ " total, " ++ show rate ++ " per second"
-      timer mon (total + 1) newtime
-    else
-      timer mon (total + 1) time
+timer :: Int -> UTCTime -> Conduit FilePath IO FilePath
+timer total time = do
+  x' <- await
+  case x' of
+    Just x -> do
+      yield x
+      if total `mod` mark == 0
+        then do
+          newtime <- liftIO getCurrentTime
+          let diff = diffUTCTime newtime time
+              rate = truncate $ fromIntegral mark / diff :: Integer
+          liftIO $ putStrLn $ show total ++ " total, " ++ show rate ++ " per second"
+          timer (total + 1) newtime
+        else
+          timer (total + 1) time
+    Nothing -> return ()
+
+source :: FilePath -> Source IO FilePath
+source path = do
+  contents <- liftIO $ map (path </>) . filter (not . isPrefixOf ".") <$> getDirectoryContents path
+  statted <- liftIO $ zip contents <$> mapConcurrently getFileStatus contents
+  let (dirs', files') = partition (isDirectory . snd) statted
+      dirs  = map fst dirs'
+      files = map fst files'
+  DCC.yieldMany files
+  mapM_ source dirs
+
+crawl :: FilePath -> IO ()
+crawl path = do
+  --let source = sourceDirectoryDeep False path
+  time <- getCurrentTime
+  source path =$=& timer 1 time $$& sink
 
 main :: IO ()
 main = do
   args <- getArgs
-  chan <- newChan
-  monitor <- dupChan chan
-  time <- getCurrentTime
-  _ <- forkIO $ timer monitor 1 time
   case args of
-    [path] -> writeChan chan path
-    _      -> writeChan chan "."
-  concurrency <- getNumCapabilities
-  _ <- mapConcurrently (worker chan) [1..concurrency*3]
+    [path] -> crawl path
+    _      -> crawl "."
   return ()
